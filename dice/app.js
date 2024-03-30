267245b98acbacd76732d84976335b53de751976
incal17 <164011530+incal17@users.noreply.github.com>
2024-03-30 18:45:47 +0900

    Add files via upload

--- dice/app.js
+++ dice/app.js
@@ -1,107 +1,112 @@
 ﻿
-let diceCount = 1; // 既存のサイコロをカウント
+let diceCount = 0; // 既存のサイコロをカウント
+let activeDice = {}; // キーとしてダイスのID、値としてアクティブ状態を持つオブジェクト
 function allRollDice() {
     console.log("allRollDice was clicked");
 
-    const diceContainer = document.getElementById('diceContainer');
-    const dices = diceContainer.querySelectorAll('.dice');
-
-    console.log("diceContainer : " + diceContainer);
-    console.log("diceContainer length: " + diceContainer.length);
-
-    dices.forEach((dice, index) => {
-        rollDice(index + 1); // 0ではなく1から数え始めるために index + 1 を使う
-        console.log("Rolled dice number " + (index + 1));
+    Object.keys(activeDice).forEach((diceId) => {
+        if (activeDice[diceId]) { // ダイスがアクティブな場合のみrollDiceを実行
+            rollDice(diceId);
+            console.log("Rolled dice number " + diceId);
+        }
     });
-    //}
 }
-function rollDice(diceNumber) {
-    var dice = document.getElementById(`dice_${diceNumber}`);
-    console.log("diceNumber" + diceNumber)
+function rollDice(diceId) {
+    console.log("rollDice")
 
-    let randomDiceNum = getRandomNumber(1, 7)
-    var currentX = parseInt(dice.getAttribute('data-x')) || 0;
+    if (activeDice[diceId]) { // ダイスがアクティブな場合のみ処理を実行
+        var dice = document.getElementById(`dice_${diceId}`);
+        console.log("diceNumber" + diceId)
+        if (!dice) {
+            console.log(`Dice with ID dice_${diceId} does not exist.`);
+            return;
+        }
+        let randomDiceNum = getRandomNumber(1, 7)
+        var currentX = parseInt(dice.getAttribute('data-x')) || 0;
 
-    var xRand, yRand
-    switch (randomDiceNum) {
-        case 1:
-            // サイコロを1の面が上に来るように回転
-            console.log(randomDiceNum);
-            if (currentX <= 0) {
-                xRand = 360;
-                yRand = 360;
-            } else {
-                xRand = 0;
-                yRand = 0;
-            }
-            break;
-        case 2:
-            // サイコロを2の面が上に来るように回転
-            if (currentX <= 0) {
-                xRand = 360;
-                yRand = 630;
-            } else {
-                xRand = 0;
-                yRand = -90;
-            }
+        var xRand, yRand
+        switch (randomDiceNum) {
+            case 1:
+                // サイコロを1の面が上に来るように回転
+                //console.log(randomDiceNum);
+                if (currentX <= 0) {
+                    xRand = 360;
+                    yRand = 360;
+                } else {
+                    xRand = 0;
+                    yRand = 0;
+                }
+                break;
 
-            console.log("サイコロは2の面が上");
-            break;
-        case 3:
-            // サイコロを3の面が上に来るように回転
-            if (currentX <= 0) {
-                xRand = 630;
-                yRand = 360;
-            } else {
-                xRand = -90;
-                yRand = 0;
-            }
-            console.log("サイコロは3の面が上");
-            break;
-        case 4:
-            // サイコロを4の面が上に来るように回転
-            if (currentX <= 0) {
-                xRand = 450;
-                yRand = 360;
-            } else {
-                xRand = -270;
-                yRand = 0;
-            }
-            console.log("サイコロは4の面が上");
-            break;
-        case 5:
-            if (currentX <= 0) {
-                xRand = 360;
-                yRand = 450;
-            } else {
-                xRand = 0;
-                yRand = -270;
-            }
-            // サイコロを5の面が上に来るように回転
-            console.log("サイコロは5の面が上");
-            break;
-        case 6:
-            if (currentX <= 0) {
-                xRand = 360;
-                yRand = 540;
-            } else {
-                xRand = 0;
-                yRand = -180;
-            }
-            // サイコロを6の面が上に来るように回転
-            console.log("サイコロは6の面が上");
-            break;
+            case 2:
+                // サイコロを2の面が上に来るように回転
+                if (currentX <= 0) {
+                    xRand = 360;
+                    yRand = 630;
+                } else {
+                    xRand = 0;
+                    yRand = -90;
+                }
+
+                //console.log("サイコロは2の面が上");
+                break;
+            case 3:
+                // サイコロを3の面が上に来るように回転
+                if (currentX <= 0) {
+                    xRand = 630;
+                    yRand = 360;
+                } else {
+                    xRand = -90;
+                    yRand = 0;
+                }
+                //console.log("サイコロは3の面が上");
+                break;
+            case 4:
+                // サイコロを4の面が上に来るように回転
+                if (currentX <= 0) {
+                    xRand = 450;
+                    yRand = 360;
+                } else {
+                    xRand = -270;
+                    yRand = 0;
+                }
+                //console.log("サイコロは4の面が上");
+                break;
+            case 5:
+                if (currentX <= 0) {
+                    xRand = 360;
+                    yRand = 450;
+                } else {
+                    xRand = 0;
+                    yRand = -270;
+                }
+                // サイコロを5の面が上に来るように回転
+                //console.log("サイコロは5の面が上");
+                break;
+            case 6:
+                if (currentX <= 0) {
+                    xRand = 360;
+                    yRand = 540;
+                } else {
+                    xRand = 0;
+                    yRand = -180;
+                }
+                // サイコロを6の面が上に来るように回転
+                //console.log("サイコロは6の面が上");
+                break;
+        }
+        dice.setAttribute('diceFaceNum', randomDiceNum);
+
+        currentX = xRand;
+
+
+        dice.setAttribute('data-x', xRand);
+
+        dice.style.transform = `rotateX(${xRand}deg) rotateY(${yRand}deg )`;
+
+    }else {
+        console.log(`Dice with ID dice_${diceId} is not active.`);
     }
-    dice.setAttribute('diceFaceNum', randomDiceNum);
-
-    currentX = xRand;
-
-
-    dice.setAttribute('data-x', xRand);
-
-    dice.style.transform = `rotateX(${xRand}deg) rotateY(${yRand}deg )`;
-
-
 }
 
 function getRandomNumber(min, max) {
@@ -114,17 +119,16 @@ function reSizeDice() {
         const size = dice.offsetWidth; // サイコロの幅（または高さ）を取得
         const halfSize = size / 2; // サイコロの幅（または高さ）の半分を取得
         dice.style.setProperty('--dice-size', `${size}px`); // CSS変数を更新
-        console.log("size : " + size);
-        console.log("halfSize : " + halfSize);
+        //console.log("size : " + size);
+        //console.log("halfSize : " + halfSize);
     });
 
 }
 function addDice() {
-    diceCount++;
     const diceContainer = document.getElementById('diceContainer');
     const newDice = document.createElement('div');
     const currentDiceNumber = diceCount; // 現在のサイコロの番号をキャプチャ
-    newDice.id = `dice_${diceCount}`;
+    newDice.id = `dice_${currentDiceNumber}`;
     newDice.className = 'dice';
     newDice.innerHTML = `
         <div class="dice-face one"><span class="dot"></span></div>
@@ -134,13 +138,61 @@ function addDice() {
         <div class="dice-face five"><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span></div>
         <div class="dice-face six"><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span></div><div class="dice-face seven"></div><div class="dice-face eight"></div><div class="dice-face nine"></div>`;
 
+    console.log(currentDiceNumber);
+    // ダイスをアクティブとしてマーク
+    activeDice[currentDiceNumber] = true;
+
     newDice.setAttribute('data-x', 0);
     newDice.setAttribute('data-y', 0);
-    newDice.onclick = function () { rollDice(currentDiceNumber); };
+    let longPressTimer;
+
+    newDice.onmousedown = function (e) {
+        e.stopPropagation();
+        longPressTimer = window.setTimeout(function () {
+            diceContainer.removeChild(newDice);
+            delete activeDice[currentDiceNumber];
+            console.log(`Dice ${currentDiceNumber} removed`);
+            reSizeDice();
+        }, 1000); // 1秒間長押しで削除
+    };
+
+    newDice.ontouchstart = function (e) {
+        e.stopPropagation();
+        longPressTimer = window.setTimeout(function () {
+            diceContainer.removeChild(newDice);
+            delete activeDice[currentDiceNumber];
+            console.log(`Dice ${currentDiceNumber} removed`);
+            reSizeDice();
+        }, 1000); // 1秒間長押しで削除
+    };
+
+    newDice.onmouseup = newDice.ontouchend = function (e) {
+        clearTimeout(longPressTimer);
+        if (e.type === 'mouseup') {
+            rollDice(currentDiceNumber);
+        }
+    };
+
+    newDice.ontouchmove = function (e) {
+        clearTimeout(longPressTimer);
+    };
+
+    newDice.onclick = function () {
+        if (!longPressTimer) {
+            rollDice(currentDiceNumber);
+        }
+    };
+
+   
     diceContainer.appendChild(newDice);
     reSizeDice();
+    diceCount++;
+
+
 
 }
+
+
 window.addEventListener('resize', reSizeDice);
 // 初期設定として左下に配置
 window.onload = function () {
@@ -154,5 +206,6 @@ window.onload = function () {
     //dice.style.top = `${topPosition}px`;
     let width = document.documentElement.clientWidth;  // ビューポートの幅
     let height = document.documentElement.clientHeight; // ビューポートの高さ
+    addDice();
     reSizeDice();
 };
